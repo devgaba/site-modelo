d16f8ba1e6afcbaea514a76bac315e5c3b545bcd
Gabriel Henrique <souza.rocha2000@gmail.com>
2026-01-16 22:35:40 -0300

    terceiro teste

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://github.com/devgaba/site-modelo.git'
+  plugins: [react()]
 })
